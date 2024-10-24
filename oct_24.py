623c59c36ad7594e32fd60a8330419c1936dc396
Jayaprakash118 <asadijayaprakash@gmail.com>
2024-10-24 15:50:52 +0530

    Add files via upload

---
+++ oct_24.py
@@ -0,0 +1,35 @@
+# operators
+# arthimatice
+a = 2
+b = 4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a==b)
+print(a**b)     #exponential operator
+#relational operators
+#<,>,<=,>=,!=
+print(a!=b)
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+print(a==b) 
+# exchanging values between 2 variables
+x,y = 10,20
+x,y=y,x #before and after sweeping values without 3 variable
+print(x,y)
+#logical operators
+# and  or not 
+a = True
+b = False
+print(a<b)
+print(a>b)
+print((a<b) and (a<b))
+print((a>b) and (a>b))
+print((a<b)  or (a<b))
+print((a>b) or  (a>b))
+print( not a )
